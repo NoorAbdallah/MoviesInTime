5e72bd1ddc1ff18aa1dc718cc497497155bf25b7
Noor A <NoorA@gmail.com>
2016-06-10 09:48:24 -0400

    init commit

---
+++ css.cs
@@ -0,0 +1,71 @@
+.jumbotron{
+   text-align:center;
+   font-family:"Edwardian Script ITC";
+   color:black;
+   height:190px;
+   margin:0 auto;
+   background-color:white;
+
+   }
+.navbar-brand{
+   font-size:30px;
+   font-weight:bold;
+   color:black;
+   }
+.navbar{
+    margin:0 auto;
+    font-size:23px;
+    background-color:#6699CC;
+    font-family:"Edwardian Script ITC";
+    font-weight:bold;
+    background-color:black;
+   }
+  .item img{
+  display:inline-block;
+  margin:0 auto;
+  }
+  .container{
+  width:65%;
+  margin:0 auto;
+  background-color:white;
+  opacity:0.9;
+  
+  }
+  .carousel{
+  padding:0 auto;
+  background-color:white;
+  }
+  .navbar a{
+  color:white;
+  }
+  .row{
+  font-size:20px;
+  }
+  #movies{
+  font-size:20px;
+  }
+ p{
+  text-align:center;
+  font-size:20px;
+  }
+  h1{
+  text-align:center;
+  }
+  body{
+  background-image:url('art.png')
+  }
+  .three{
+   width:65%;
+  margin:0 auto;
+  background-color:white;  
+  }
+  #hello1{
+  float:right;
+  }
+  #mymodal2{
+  float:right;
+  }
+  #four{
+  width:320px;
+  height:0 auto;
+  }
